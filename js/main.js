93c838ef255e0e6c730a21a2626687bb63b7eded
tomoya ueda <to.ueda82@gmail.com>
2012-03-02 00:43:10 +0900

    up し忘れた

---
+++ js/main.js
@@ -0,0 +1,33 @@
+(function(){
+  var sandbox = new function(){
+    var window = null;
+    var document = null;
+    var alert = null;
+    var logs='';
+    this.do = function(code){
+      logs='';
+      with(this){
+        eval(code);
+      }
+    };
+    this.log =function(output){
+        logs+=output + "\n";
+    };
+    this.outputLog =function(){
+       return logs; 
+    };
+  }();
+
+  var obj = document.getElementById("runJavascript");
+  var f = function () {
+    var code = document.getElementById("inputarea").value;
+    sandbox.do(code);
+    document.getElementById("outputarea").value = sandbox.outputLog();
+  };
+
+  if (obj.attachEvent){
+    obj.attachEvent('onclick', f);
+  }else {
+    obj.addEventListener('click', f, false); 
+  }
+})()
