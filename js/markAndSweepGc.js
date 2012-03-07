cbd5108902048f181131c6c2fa3a8d04c23d2774
tomoya ueda <to.ueda82@gmail.com>
2012-03-08 02:10:37 +0900

    OK・freeListを作成する。 OK・freeListを初期化する関数を作成する。 OK・オブジェクトクラスOをnewした際、chunk関数が動く。 OK・chunk関数はfreeListから空いているheapの要素を取得しオブジェクトを格納する

--- js/markAndSweepGc.js
+++ js/markAndSweepGc.js
@@ -1,33 +1,33 @@
 var gc = {
   HEAP_SIZE : 100,
-  heap : new Array(HEAP_SIZE),
-  root : sandbox,
+  heap : new Array(),
+  root : window.sandbox,
   freeList:null,
-  chunk :function(){
-    if(freeList.length==0){
+  //object set heap
+  chunk :function(obj){
+    var l=this.freeList;
+    if(l.length==0){
        alert('heap is nothing!'); 
     }
-    var i =freeList[0];
-
-    return i; 
-
-  
+    var i =l[0];
+    l.shift();
+    this.heap[i] = obj;
+    return; 
   },
   newFreeList:function(){
     this.freeList = new Array();  
-    for (var i=0; i < this.HEAP_SIZE.length; i++) {
-       this.freeList.push(i); 
+    for (var i=0; i < this.HEAP_SIZE; i++) {
+      this.freeList.push(i); 
     };
   }
 }
 
-(function(){
-    gc.newFreeList();
-})();
+gc.newFreeList();
 
 //O is GC target Object
 var O = function(v){
-  var isGcObject = true;
-  var mark = false;
-  var value = v;
+  this.isGcObject = true;
+  this.mark = false;
+  this.value = v;
+  gc.chunk(this);
 }
